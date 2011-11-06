a1f94809551b08ea22863b75a98255e14d8ac974
Dominique Guardiola <contact@quinode.fr>
2011-11-06 20:23:00 +0100

    setuptools package

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('djaloha').__version__
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='Djaloha',
+    version = VERSION,
+    description='Django integration for aloha HTML5 editor',
+    packages=['djaloha','djaloha.templatetags'],
+    package_data = { "djaloha": [   'local/fr/LC_MESSAGES/django.mo',
+                                    'local/fr/LC_MESSAGES/django.mo',
+                                    'static/aloha/'],
+                    },
+    include_package_data=True,
+    author='Luc Jean',
+    author_email='ljean@apidev.fr',
+    license='BSD',
+    #long_description=read('README.txt'),
+    download_url='git://github.com/quinode/djaloha.git',
+)
+

5d245e33f69ab65fe671d7f008d4a32a41eb11d0
Dominique Guardiola <dguardiola@quinode.fr>
2012-06-28 12:45:01 +0200

    ok nouveau settings DJALOHA_JQUERY, mais le fallback indique un 1.7.2.js qui existe pas, j'ai remis celui qui est dispo dans coop-cms

--- djaloha/widgets.py
+++ djaloha/widgets.py
@@ -11,27 +11,27 @@ class AlohaInput(TextInput):
     Text widget with aloha html editor
     requires floppyforms to be installed
     """
-    
+
     template_name='djaloha/alohainput.html'
-    
+
     def __init__(self, *args, **kwargs):
         kwargs.pop('text_color_plugin', None) # for compatibility with previous versions
         self.aloha_plugins = kwargs.pop('aloha_plugins', None)
         self.extra_aloha_plugins = kwargs.pop('extra_aloha_plugins', None)
         self.aloha_init_url = kwargs.pop('aloha_init_url', None)
         super(AlohaInput, self).__init__(*args, **kwargs)
-    
+
     def _get_media(self):
         """
         return code for inserting required js and css files
         need aloha , plugins and initialization
         include the 3rd party ImagePlugin from tapo: make possible to drag-and-drop and edit images
         """
-        
+
         try:
-            custom_jquery = getattr(settings, 'DJALOHA_JQUERY', "js/jquery.1.7.2.js")
+            custom_jquery = getattr(settings, 'DJALOHA_JQUERY', "js/jquery.1.7.1.min.js") # le fallback est celui de coop-cms ?
             aloha_init_url = self.aloha_init_url or getattr(settings, 'DJALOHA_INIT_URL', None) or reverse('aloha_init')
-            
+
             aloha_plugins = self.aloha_plugins
             if not aloha_plugins:
                 aloha_plugins = getattr(settings, 'DJALOHA_PLUGINS', None)
@@ -54,22 +54,22 @@ class AlohaInput(TextInput):
                 )
             if self.extra_aloha_plugins:
                 aloha_plugins = tuple(aloha_plugins) + tuple(self.extra_aloha_plugins)
-            
+
             css = {
                 'all': (
                     "aloha.0.20/css/aloha.css",
                 )
             }
-        
+
             js = (
                 custom_jquery,
                 # Yes I know this is very dirty but the better (less bad) solution so-far
                 u'aloha.0.20/lib/aloha.js" data-aloha-plugins="{0}'.format(u",".join(aloha_plugins)),
                 aloha_init_url,
             )
-            
+
             return Media(css=css, js=js)
         except Exception, msg:
-            print '## AlohaInput._get_media Error ', msg 
-    
+            print '## AlohaInput._get_media Error ', msg
+
     media = property(_get_media)
